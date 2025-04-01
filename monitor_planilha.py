c709217d67b6509fe5ce4bbe3ea208ac2d30d2c3
CaioAnjos30 <caioanjos.scanntech@gmail.com>
2025-04-01 15:33:40 -0300

    monitor_planilha.py

--- monitor_planilha.py
+++ monitor_planilha.py
@@ -3,29 +3,52 @@ import json
 import requests
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from datetime import datetime
 
-# === Configurações ===
-ID_ARQUIVO = '1WjKXeS7lXkWW8rEFBdLRiBtAWEp-5vUT'
+ID_ARQUIVO = '1WjKXeS7lXkWW8rEFBdLRiBtAWEp-5vUT'  # <-- Coloque o ID do seu arquivo
 ARQUIVO_ULTIMA_MODIFICACAO = 'ultima_modificacao.txt'
 TOKEN = '7498773442:AAEO8ihxIP18JtFSrO_6UGeC8VPtIJVH2rU'
 CHAT_ID = '8142521159'
 
-def get_modified_time():
+def enviar_telegram(mensagem):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    resposta = requests.post(url, data={'chat_id': CHAT_ID, 'text': mensagem})
+    if resposta.status_code == 200:
+        print("✅ Telegram enviado!")
+    else:
+        print(f"❌ Falha ao enviar: {resposta.text}")
+
+def get_ultima_atividade():
     google_creds_json = os.environ.get('GOOGLE_CREDENTIALS')
     if not google_creds_json:
         raise ValueError("❌ Variável de ambiente GOOGLE_CREDENTIALS não encontrada!")
 
     cred_dict = json.loads(google_creds_json)
-    scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+    scopes = ['https://www.googleapis.com/auth/drive.activity.readonly']
     creds = service_account.Credentials.from_service_account_info(cred_dict, scopes=scopes)
-    service = build('drive', 'v3', credentials=creds)
+    service = build('driveactivity', 'v2', credentials=creds)
 
-    file = service.files().get(
-        fileId=ID_ARQUIVO,
-        fields='modifiedTime'
-    ).execute()
+    body = {
+        "itemName": f"items/{ID_ARQUIVO}",
+        "pageSize": 1
+    }
 
-    return file['modifiedTime']
+    response = service.activity().query(body=body).execute()
+    atividades = response.get('activities', [])
+
+    if not atividades:
+        raise ValueError("❌ Nenhuma atividade encontrada para o arquivo.")
+
+    atividade = atividades[0]
+    user = atividade['actors'][0].get('user', {}).get('knownUser', {}).get('personName', 'Desconhecido')
+    email = atividade['actors'][0].get('user', {}).get('knownUser', {}).get('personName', 'sem email')
+
+    timestamp = atividade.get('timestamp')
+    if not timestamp:
+        timestamp = atividade['timeRange']['endTime']
+
+    return timestamp, email
+
 
 
 def enviar_telegram(mensagem):
