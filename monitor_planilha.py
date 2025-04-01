556f99e6771c4323e1943be3ea4535975af205ae
CaioAnjos30 <caioanjos.scanntech@gmail.com>
2025-04-01 15:14:11 -0300

    Update monitor_planilha.py

--- monitor_planilha.py
+++ monitor_planilha.py
@@ -10,25 +10,24 @@ ARQUIVO_ULTIMA_MODIFICACAO = 'ultima_modificacao.txt'
 TOKEN = '7498773442:AAEO8ihxIP18JtFSrO_6UGeC8VPtIJVH2rU'
 CHAT_ID = '8142521159'
 
-def get_modified_time():
-    # Lê credencial da variável de ambiente
+def get_modified_info():
     google_creds_json = os.environ.get('GOOGLE_CREDENTIALS')
-
     if not google_creds_json:
         raise ValueError("❌ Variável de ambiente GOOGLE_CREDENTIALS não encontrada!")
 
-    # Converte string JSON para dicionário
     cred_dict = json.loads(google_creds_json)
-
     scopes = ['https://www.googleapis.com/auth/drive.metadata.readonly']
     creds = service_account.Credentials.from_service_account_info(cred_dict, scopes=scopes)
     service = build('drive', 'v3', credentials=creds)
 
-    file = service.files().get(fileId=ID_ARQUIVO, fields='modifiedTime').execute()
-    return file['modifiedTime']
+    file = service.files().get(
+        fileId=ID_ARQUIVO,
+        fields='modifiedTime, lastModifyingUser(displayName)'
+    ).execute()
+
+    return file['modifiedTime'], file.get('lastModifyingUser', {}).get('displayName', 'Desconhecido')
 
 def enviar_telegram(mensagem):
     url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     resposta = requests.post(url, data={'chat_id': CHAT_ID, 'text': mensagem})
     print("✅ Telegram enviado!" if resposta.status_code == 200 else f"❌ Erro: {resposta.text}")
-
