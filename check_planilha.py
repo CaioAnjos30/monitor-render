c3272585c65d3f2aea4a0c009a66300b9c096541
CaioAnjos30 <caioanjos.scanntech@gmail.com>
2025-04-01 15:34:07 -0300

    check_planilha.py

--- check_planilha.py
+++ check_planilha.py
@@ -1,11 +1,11 @@
-from monitor_planilha import get_modified_time, enviar_telegram
 import os
+from monitor_planilha import get_ultima_atividade, enviar_telegram
 
 ARQUIVO_ULTIMA_MODIFICACAO = 'ultima_modificacao.txt'
 
 def main():
     try:
-        atual = get_modified_time()
+        horario_atual, usuario = get_ultima_atividade()
 
         if os.path.exists(ARQUIVO_ULTIMA_MODIFICACAO):
             with open(ARQUIVO_ULTIMA_MODIFICACAO, 'r') as f:
@@ -13,19 +13,19 @@ def main():
         else:
             ultima = ""
 
-        if atual != ultima:
+        if horario_atual != ultima:
             mensagem = (
-                "📢 A planilha base foi atualizada com sucesso! ✅\n"
-                f"🕒 Horário: {atual}"
+                "📢 A planilha foi modificada!\n"
+                f"🧑‍💼 Quem: {usuario}\n"
+                f"🕒 Quando: {horario_atual}"
             )
             enviar_telegram(mensagem)
             with open(ARQUIVO_ULTIMA_MODIFICACAO, 'w') as f:
-                f.write(atual)
+                f.write(horario_atual)
         else:
-            print("🕒 Sem alteração detectada.")
+            print("🕒 Nenhuma modificação detectada.")
     except Exception as e:
         enviar_telegram(f"⚠️ Erro no monitoramento: ❌ {e}")
-
 
 if __name__ == '__main__':
     main()
